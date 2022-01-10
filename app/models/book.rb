bba368c30e6ac19e3a5dfd8a891a6a54f6949c1a
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-10 17:56:48 -0300

    adicionando casos de teste para o modelo book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
   belongs_to :language
+
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :ISBN, presence: true
 end
