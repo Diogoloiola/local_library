5880afffc85d30b18a89bf038d1a3614e6712445
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-13 11:23:06 -0300

    corrigingo caso de teste da rota books

--- spec/routing/v1/books_routing_spec.rb
+++ spec/routing/v1/books_routing_spec.rb
@@ -1,30 +1,29 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe V1::BooksController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/v1/books").to route_to("v1/books#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/v1/books').to route_to('v1/books#index', format: :json)
     end
 
-    it "routes to #show" do
-      expect(get: "/v1/books/1").to route_to("v1/books#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/v1/books/1').to route_to('v1/books#show', id: '1', format: :json)
     end
 
-
-    it "routes to #create" do
-      expect(post: "/v1/books").to route_to("v1/books#create")
+    it 'routes to #create' do
+      expect(post: '/v1/books').to route_to('v1/books#create', format: :json)
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/v1/books/1").to route_to("v1/books#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/v1/books/1').to route_to('v1/books#update', id: '1', format: :json)
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/v1/books/1").to route_to("v1/books#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/v1/books/1').to route_to('v1/books#update', id: '1', format: :json)
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/v1/books/1").to route_to("v1/books#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/v1/books/1').to route_to('v1/books#destroy', id: '1', format: :json)
     end
   end
 end
