5c3dc3fcfaa4cf6671518b15a70acccadba9a6da
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-13 16:26:56 -0300

    alterando o arquivo de rota para suportar mulitplos usuários

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth/v1/user'
 
-  mount_devise_token_auth_for 'Admin', at: 'auth'
+  mount_devise_token_auth_for 'Admin', at: 'auth/v1/admin'
 
   namespace :admins, defaults: { format: :json } do
     namespace :v1 do
