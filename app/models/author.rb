f012ac01d709f4a09e47ea7adc8a2b9f19396633
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-13 13:40:06 -0300

    adicionando condição para que exista apenas um único nome de autor

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
-  validates :name, presence: true
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false, message: 'Esse nome já consta em nossa base, tente outro nome' }
   validates :date_of_birth, presence: true
   validates :date_of_death, presence: true
 end
