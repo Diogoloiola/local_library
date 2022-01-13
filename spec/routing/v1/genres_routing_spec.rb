aa22d53a865769bc1cad59fb978ae4e783c9ec6f
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-13 11:23:50 -0300

    corrigingo caso de teste da rota genres

--- spec/routing/v1/genres_routing_spec.rb
+++ spec/routing/v1/genres_routing_spec.rb
@@ -1,30 +1,29 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe V1::GenresController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/v1/genres").to route_to("v1/genres#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/v1/genres').to route_to('v1/genres#index', format: :json)
     end
 
-    it "routes to #show" do
-      expect(get: "/v1/genres/1").to route_to("v1/genres#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/v1/genres/1').to route_to('v1/genres#show', id: '1', format: :json)
     end
 
-
-    it "routes to #create" do
-      expect(post: "/v1/genres").to route_to("v1/genres#create")
+    it 'routes to #create' do
+      expect(post: '/v1/genres').to route_to('v1/genres#create', format: :json)
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/v1/genres/1").to route_to("v1/genres#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/v1/genres/1').to route_to('v1/genres#update', id: '1', format: :json)
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/v1/genres/1").to route_to("v1/genres#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/v1/genres/1').to route_to('v1/genres#update', id: '1', format: :json)
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/v1/genres/1").to route_to("v1/genres#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/v1/genres/1').to route_to('v1/genres#destroy', id: '1', format: :json)
     end
   end
 end
