99ba3b5fe28b31291cd28bb232d6f2bc6140150a
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-13 11:24:44 -0300

    corrigingo caso de teste da rota languages

--- spec/routing/v1/languages_routing_spec.rb
+++ spec/routing/v1/languages_routing_spec.rb
@@ -1,30 +1,29 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe V1::LanguagesController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/v1/languages").to route_to("v1/languages#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/v1/languages').to route_to('v1/languages#index', format: :json)
     end
 
-    it "routes to #show" do
-      expect(get: "/v1/languages/1").to route_to("v1/languages#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/v1/languages/1').to route_to('v1/languages#show', id: '1', format: :json)
     end
 
-
-    it "routes to #create" do
-      expect(post: "/v1/languages").to route_to("v1/languages#create")
+    it 'routes to #create' do
+      expect(post: '/v1/languages').to route_to('v1/languages#create', format: :json)
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/v1/languages/1").to route_to("v1/languages#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/v1/languages/1').to route_to('v1/languages#update', id: '1', format: :json)
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/v1/languages/1").to route_to("v1/languages#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/v1/languages/1').to route_to('v1/languages#update', id: '1', format: :json)
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/v1/languages/1").to route_to("v1/languages#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/v1/languages/1').to route_to('v1/languages#destroy', id: '1', format: :json)
     end
   end
 end
